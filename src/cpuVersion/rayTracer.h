94b5ad06bc633dcd111b47bf417c0ae6608fbeb9
jaafar <jaafer06@hotmail.fr>
2021-09-26 11:22:28 +0200

    upgrated to cpp17 parrallel ray casting diffuse lighting test

--- src/cpuVersion/rayTracer.h
+++ src/cpuVersion/rayTracer.h
@@ -1,6 +1,22 @@
 #include "Eigen/Dense"
+#include <execution>
+#include <algorithm>
+#include <execution>
 
 using namespace Eigen;
+
+static Vector3f light(1, 1, 1);
+
+struct Color {
+    float r;
+    float g;
+    float b;
+    void operator=(const Vector3f& other) {
+        r = other[0];
+        g = other[1];
+        b = other[2];
+    }
+};
 
 class ray {
     public:
@@ -21,59 +37,89 @@ class ray {
         Vector3f dir;
 };
 
-inline bool hit_sphere(const Vector3f& center, double radius, const ray& r) {
+struct hit_record {
+    Vector3f p;
+    Vector3f normal;
+    double t;
+};
+
+class hittable {
+    public:
+        virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const = 0;
+};
+
+inline float hit_sphere(const Vector3f& center, double radius, const ray& r) {
     Vector3f oc = r.origin() - center;
     auto a = r.direction().dot(r.direction());
     auto b = 2.0 * oc.dot(r.direction());
     auto c = oc.dot(oc) - radius*radius;
     auto discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - sqrt(b*b-4*a *c) ) / 2*a;
+    }
+}
+
+inline Vector3f sphereNormal(const Vector3f& center, const Vector3f& at) {
+    return (at - center ).normalized();
 }
 
 Vector3f ray_color(const ray& r) {
-     if (hit_sphere(Vector3f(0,0,-3), 0.5, r))
-        return Vector3f(1, 0, 0);
+
+    float t = hit_sphere(Vector3f(0,0,-2), 0.5, r);
+    if (t >= 0.0) {
+        Vector3f normal = sphereNormal(Vector3f(0,0,-3), r.at(t));
+        Vector3f surfaceToLight = (light - r.at(t)).normalized();
+        float intensity = surfaceToLight.dot(normal);
+        return  0.7*(Vector3f(1, 0, 0)* std::max(intensity, 0.f)) + 0.3 * Vector3f(1, 0, 0);
+    }
+
     Vector3f unit_direction = r.direction().normalized();
-    float t = 0.5*(unit_direction[1] + 1);
-    return (1.0-t) * Vector3f(1.0, 1.0, 1.0) + t*Vector3f(0.5, 0.7, 1.0);
+    t = 0.5*(unit_direction[1] + 1);
+    return (1.0 - t) * Vector3f(1.0, 1.0, 1.0) + t * Vector3f(0.5, 0.7, 1.0);
 }
 
 class Camera {
 public:
-    Camera(Vector3f position, Vector3f up, Vector3f right, float focalLength) 
-    : position(position), focalLength(focalLength), up(up), right(right) 
-    {
 
-    }
-
-    Camera(float* buffer, unsigned int width, unsigned int height): width(width), height(height), buffer(buffer) {
+    Camera(void* buffer, unsigned int width, unsigned int height): width(width), height(height) {
         position = Vector3f(0, 0, 0);
         focalLength = 1;
         up = Vector3f(0, 1, 0);
         right = Vector3f(1, 0, 0);
+        this->buffer = static_cast<Color*>(buffer);
+
+        worldWidth = width * 1 / sqrt(width * height);
+        worldHeight = height * 1 / sqrt(width * height);
+        upper_left = { -worldWidth / 2, worldHeight / 2, -1 };
+        du = worldWidth / (width - 1);
+        dv = worldHeight / (height - 1);
     }
 
     void render() { 
         
-        float worldWidth = width * 1/sqrt(width*height);
-        float worldHeight = height * 1/sqrt(width*height);
-        Vector3f upper_left(-worldWidth/2, worldHeight/2, 1);
+        auto p = [this](Color& pixel) {
+            int index = &pixel - buffer;
+            int i = index % width;
+            int j = index / width;
+            Vector3f pixelWorldSpace = upper_left + (du * i + du/2) * right - (dv * j + dv/2) * up ;
+            ray r(position, pixelWorldSpace - position);
+            Vector3f pixel_color = ray_color(r);
+            buffer[(height-1-j)*width+i] = pixel_color;
+        };
 
+        std::for_each(std::execution::par, buffer, buffer+width*height-1, p);
 
-        float du = worldWidth/(width-1);
-        float dv = worldHeight/(height-1);
-
-        for (int j = 0; j < height; ++j) {
-            for (int i = 0; i < width; ++i) {
-                Vector3f pixelWorldSpace = upper_left + (du * i + du/2) * right - (dv * j + dv/2) * up ;
-                ray r(position, pixelWorldSpace - position);
-                Vector3f pixel_color = ray_color(r);
-                buffer[(height-1-j)*width*3+i*3] = pixel_color[0];
-                buffer[(height-1-j)*width*3+i*3+1] = pixel_color[1];
-                buffer[(height-1-j)*width*3+i*3+2] = pixel_color[2];
-            }
-
-        }
+         //for (int j = 0; j < height; ++j) {
+         //    for (int i = 0; i < width; ++i) {
+         //        Vector3f pixelWorldSpace = upper_left + (du * i + du/2) * right - (dv * j + dv/2) * up ;
+         //        ray r(position, pixelWorldSpace - position);
+         //        Vector3f pixel_color = ray_color(r);
+         //        buffer[(height-1-j)*width+i] = pixel_color;
+         //    }
+         //}
+        light += Vector3f(0, 0, -0.01);
     }
 
 private:
@@ -83,10 +129,11 @@ private:
     float focalLength;
     unsigned int width;
     unsigned int height;
-    float* buffer;
+    float worldWidth;
+    float worldHeight;
+    Vector3f upper_left;
+    float du, dv;
+    Color* buffer;
 };
-
-
-
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <time.h>
 #include "cpuVersion/rayTracer.h"
+#include <chrono>
 
 static void error_callback(int error, const char *description)
 {
@@ -104,7 +105,26 @@ int main()
                       0, 0, width, height,         // dst rect
                       GL_COLOR_BUFFER_BIT,         // buffer mask
                       GL_LINEAR);                  // scale filter
-                
+        
+        using std::chrono::high_resolution_clock;
+        using std::chrono::duration_cast;
+        using std::chrono::duration;
+        using std::chrono::milliseconds;
+
+        auto t1 = high_resolution_clock::now();
+
+        camera.render();
+        auto t2 = high_resolution_clock::now();
+
+    /* Getting number of milliseconds as an integer. */
+        auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+        /* Getting number of milliseconds as a double. */
+        duration<double, std::milli> ms_double = t2 - t1;
+
+        std::cout << ms_int.count() << "ms\n";
+        std::cout << ms_double.count() << "ms";
+
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
